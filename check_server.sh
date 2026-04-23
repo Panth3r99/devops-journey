4a849a941d22127c3ddd6d849cc1104528e4331a
Daivik <daivikmurali@gmail.com>
2026-04-23 23:51:25 +0530

    Made script cross-platforms (Windows + Linux)

--- check_server.sh
+++ check_server.sh
@@ -2,17 +2,24 @@
 
 echo "Starting server checks..."
 
+# Detect OS
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    ping_cmd="ping -n 2"
+else
+    ping_cmd="ping -c 2"
+fi
+
 while read server
 do
-    if ping -n 2 "$server" > /dev/null
+    if $ping_cmd "$server" > /dev/null
     then
         status="UP"
     else
         status="DOWN"
-        echo "⚠️ ALERT: $server is DOWN"
+        echo "ALERT: $server is DOWN"
     fi
 
-    echo "[$(date)] $server status: $status"
+    echo "$server is $status"
     echo "$(date) - $server is $status" >> monitor.log
 
 done < servers.txt
